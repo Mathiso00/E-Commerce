e811dae28bcd20d75406897c4fb60c6afa39ae72
angemarchand <ange.marchand@epitech.eu>
2023-04-04 12:04:04 +0200

    update gitignroe

---
+++ client/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <v-col>
+      <h1 class="text-red text-center"> Welcome to our merchant site </h1>
+    </v-col>
+
+  </v-container>
+</template>
+
+<script setup lang="ts">
+
+</script>
