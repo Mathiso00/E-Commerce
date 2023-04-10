508000d3dce522f27c4d6db1ec73a4392ae3fb66
angemarchand <ange.marchand@epitech.eu>
2023-04-10 19:59:12 +0200

    feat(some products and rating component in home page)30-home-page-front

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,12 +1,14 @@
 <template>
-  <v-app>
+  <v-app style="background-color: #e2e2e2">
     <Navbar />
     <v-main>
       <slot />
     </v-main>
+    <Footeer />
   </v-app>
 </template>
 
 <script setup lang="ts">
 import Navbar from "~/components/Navbar.vue";
+import Footeer from "~/components/Footer.vue";
 </script>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,10 +1,104 @@
 <template>
   <div>
     <Carousel />
+    <div >
+      <h1 class="text-center font-weight-light my-5">The most popular products</h1>
+      <v-container
+          class="fill-height mb-10"
+          fluid
+          style="min-height: 434px"
+      >
+        <v-fade-transition mode="out-in">
+          <v-row>
+            <v-col
+                v-for="product in products"
+                :key="product"
+                cols="12"
+                sm="6"
+                md="4"
+                lg="3"
+            >
+              <v-card class="text-center">
+                <div class="float-right px-2 ml-2 bg-black">{{ product.price }} €</div>
+                <v-img height="125" cover class="bg-grey-lighten-2 " :src="product.img" />
+                <v-card-title class="text-h6">{{ product.name }}</v-card-title>
+                <v-card-subtitle class="text-body-1 mb-3">{{ product.description }}</v-card-subtitle>
+                <Rating />
+<!--                <v-card-text class="text-body-2">{{ product.price }} €</v-card-text>-->
+              </v-card>
+            </v-col>
+
+          </v-row>
+        </v-fade-transition>
+      </v-container>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import Carousel from "~/components/Carousel.vue"
+import Carousel from "~/components/Carousel.vue";
+import Bundle from "../assets/bundle.webp";
+import Mac from "../assets/mac.jpeg";
+import Souris from "../assets/souris.webp";
+import Pc from "../assets/new-pc.webp";
+import Souris2 from  '../assets/souris2.webp'
+import Boitier from '../assets/Boitier-PC-7.jpg'
+import {useDisplay} from "vuetify";
+import Rating from "~/components/Rating.vue";
+
+const { width, mobile } = useDisplay();
+let newWidth = width.value
+console.log(width.value);
+console.log(mobile.value);
+/*const getCols = () => {
+  if (width.value < 768) {
+    return 12;
+  } else if (width.value < 1024) {
+    return 6;
+  } else {
+    return 4;
+  }
+};*/
+const products: { name: string, description: string, price: number, img: string }[] = [
+  {
+    name: "IMac Pro",
+    description: "A powerful and stylish laptop for professionals and creatives.",
+    price: 1699,
+    img: Mac
+  },
+  {
+    name: "Surface Laptop 4",
+    description: "A lightweight, high-performance laptop with a touch screen for maximum productivity.",
+    price: 999,
+    img: Pc
+  },
+  {
+    name: "Computer mice",
+    description: "An elegant and modern design, with a high quality finish.",
+    price: 39,
+    img: Souris
+
+  },
+  {
+    name: "PC monitor",
+    description: "An ultra-thin laptop with impressive battery life and an AMOLED display.",
+    price: 999,
+    img: Bundle
+  },
+  {
+    name: "Dell XPS 13",
+    description: "Elegant and modern design, fits perfectly in any office or home environment",
+    price: 1099,
+    img: Boitier
+  },
+  {
+    name: "Computer mice",
+    description: "An elegant and modern design, with a high quality finish.",
+    price: 899,
+    img: Souris2
+  }
+];
+
+console.log(products)
 
 </script>
