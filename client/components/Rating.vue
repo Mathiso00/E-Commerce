9c5b88c2c45a4a7fbef89855a09a05e80b1a4f11
mathiso <mathiso@mathiso.lan>
2023-04-18 17:55:42 +0200

    Ajout de certbot

--- client/components/Rating.vue
+++ client/components/Rating.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-rating
+  <!-- <v-rating
       v-model="rating"
       color="white"
       active-color="yellow-accent-4"
@@ -8,10 +8,10 @@
       size="25"
   ></v-rating>
   <span class="text-grey-lighten-1 text-caption">
-    ({{ rating }})
-  </span>
+    {{ rating }}
+  </span> -->
 </template>
 
 <script setup lang="ts">
 const rating =  4.5;
-</script>
+</script> 
