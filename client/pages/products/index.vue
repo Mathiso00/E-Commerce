6bc7485c3f88114eb8f1ec04ce32b22eb99696a3
angemarchand <ange.marchand@epitech.eu>
2023-04-21 21:46:25 +0200

    modify productController and create product logic in nuxt application

--- client/pages/products/index.vue
+++ client/pages/products/index.vue
@@ -5,5 +5,10 @@
 </template>
 
 <script setup lang="ts">
-
+import {getProducts} from "~/composables/useProduct";
+import {getProduct} from "~/composables/useProduct";
+const data = getProducts();
+const infoProduct = getProduct(1);
+console.log("un produit: ", infoProduct)
+console.log("Les produits: ", data)
 </script>
