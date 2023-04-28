e85e80e5d6f084225558e25d70cba1b8c846cede
mathiso <mathiso@mathiso.lan>
2023-04-28 14:52:59 +0200

    fix(ansible): switch apache to nginx

--- client/composables/useAuth.ts
+++ client/composables/useAuth.ts
@@ -1,17 +1,19 @@
-
 const config = useRuntimeConfig();
 
-export async function signUp(firstname: string, lastname: string, email: string, password: string): Promise<any> {
-    try {
-        const data = await $http.$post(`https://localhost:8000/api/register`, {
-            body: {firstname, lastname, email, password}
-        })
-        return data
-    } catch (e) {
-        console.log(e);
-    }
+export async function signUp(
+  firstname: string,
+  lastname: string,
+  email: string,
+  password: string
+): Promise<any> {
+  try {
+    const data = await $http.$post(`https://127.0.0.1:8000/api/register`, {
+      body: { firstname, lastname, email, password },
+    });
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-export async function ShowUser(){
-
-}
+export async function ShowUser() {}
