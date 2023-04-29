85f9fab924eafbcb5338ca2ff7754844c76cab56
mathiso <mathiso@mathiso.lan>
2023-04-29 12:07:58 +0200

    Correctif ansible

--- client/composables/useProduct.ts
+++ client/composables/useProduct.ts
@@ -2,25 +2,22 @@ import { IProduct } from "~/types/IProduct";
 
 const config = useRuntimeConfig();
 
-export async function getProducts(): Promise<any> {
+export async function getProducts(): Promise<any>{
   try {
-    //const data = await $http.$get(`${config.apiBaseUrl}/products`);
-    const data = await $http.$get(`https://127.0.0.1:8000/api/products`);
-    //console.log(data);
-    return data;
-  } catch (e) {
-    console.log(e);
+      //const data = await $http.$get(`${config.apiBaseUrl}/products`);
+      const data = await $http.$get(`https://localhost:8000/api/products`);
+      //console.log(data);
+      return data;
+  }catch (e) {
+     console.log(e)
   }
-}
 
-export async function getProduct(productId: number): Promise<any> {
+export async function getProduct(productId: number): Promise<any>{
   try {
-    const data = await $http.$get(
-      `https://127.0.0.1:8000/api/products/${productId}`
-    );
-    console.log(data);
-    return data;
-  } catch (e) {
-    console.log(e);
+      const data = await $http.$get(`https://localhost:8000/api/products/${productId}`);
+      console.log(data);
+      return data;
+  }catch (e) {
+      console.log(e)
   }
-}
+} 
